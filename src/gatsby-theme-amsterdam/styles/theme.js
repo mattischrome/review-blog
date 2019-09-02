4821023beecd56888cbb183a1aa7931b9c7614ef
Matthew Dorey <mattischrome@gmail.com>
2019-09-02 15:37:10 +0100

    Revert colours and use new font stack

--- src/gatsby-theme-amsterdam/styles/theme.js
+++ src/gatsby-theme-amsterdam/styles/theme.js
@@ -1,20 +1,20 @@
 export default {
   colors: {
-    base: '#96CCFF',
-    secondary: '#00449E',
-    tertiary: '#357EDD',
-    highlight: '#00CCFF',
-    background: '#F6FFFE',
-    border: '#96CCFF',
-    button: '#E8FDF5',
-    text: '#00449E',
+    base: '#292929',
+    secondary: '#686461',
+    tertiary: '#958E8E',
+    highlight: '#C29967',
+    background: '#F5F0EB',
+    border: '#DCD8D3',
+    button: '#E9E4DF',
+    text: '#292929',
     code: '#E9E5E0',
   },
   fonts: {
     body:
-      '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
+      '"avenir next", avenir, sans-serif',
     heading:
-      '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
+      'baskerville, serif',
     normalWeight: '400',
     boldWeight: '600',
   },
